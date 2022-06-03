847e889f27a3b9972e280860cd9b8709b693e9c7
Satyajeet Gaurav <satyajeetmanu@gmail.com>
2022-06-03 13:49:10 +0530

    created a worker to refresh videos in the background

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -16,3 +16,26 @@
  */
 
 package com.example.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context,params: WorkerParameters): CoroutineWorker(appContext,params){
+
+    override suspend fun doWork(): Result {
+
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+
+        try{
+            repository.refreshVideos()
+            return Result.success()
+        }catch (exception: HttpException){
+            return Result.retry()
+        }
+    }
+}
